85221b0e52db03a632734ce8351e63bf2ac96bb7
Jessica <jesss8692@hotmail.com>
2024-12-21 21:19:35 +0100

    Style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
+import logo from '../assets/img/brain.png'
 function Header() {
     return(
-        <h1>Header</h1>
+        <header className="flex items-center gap-2 shadow p-4">
+           <img 
+                src={logo} 
+                alt="Brain icon." 
+                className="w-8"
+            />
+            <h1 className="text-[#F76C5E]">NeroPlaner</h1>
+        </header>
     )
 }
 

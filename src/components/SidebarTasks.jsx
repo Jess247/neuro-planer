1f35ea569f4743e92702baa79caf52f6cb5cbd5a
Jessica <jesss8692@hotmail.com>
2024-12-29 19:08:01 +0100

    Update files

--- src/components/SidebarTasks.jsx
+++ src/components/SidebarTasks.jsx
@@ -16,7 +16,7 @@ function SidebarTasks() {
     return(
         <div className={`${color} flex items-center justify-between shadow py-2 px-4 w-[90%] m-auto mb-2 rounded`}>
             <div className="flex items-center gap-2">
-                {icon.task}
+                {icon.notes}
                 <span>{taskName}</span>
             </div>
             <span className="text-xs bg-slate-100 text-sky-800 w-5 h-5 text-center pt-[1.3px] rounded-full">{amountOfTasks}</span>

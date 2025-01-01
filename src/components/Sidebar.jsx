1cda62fc1ec3d84c3b8369a6352b8f5c075e8ea7
Jessica <jesss8692@hotmail.com>
2025-01-01 14:53:43 +0100

    Update Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,20 +3,42 @@ import SearchBar from './SearchBar'
 import SidebarTask from './SidebarTask'
 import SubTask from './SubTask'
 import { MdNotes } from "react-icons/md"
+import { FaCheck } from "react-icons/fa"
+import { IoMdDoneAll } from "react-icons/io"
+import { GiAchievement } from "react-icons/gi"
 
 
 function Sidebar() {
     // state vars placeholder
     const noteCount = 0
+    const taskObjs = [
+        {
+            name: "Notes",
+            icon: MdNotes,
+            count: 0
+        },
+        {
+            name: "task",
+            icon: FaCheck,
+            count: 0
+        },
+        {
+            name: "done",
+            icon: IoMdDoneAll,
+            count: null
+        },
+        {
+            name: "achievements",
+            icon: GiAchievement,
+            count: null
+        },
+]
 
     return(
         <side className="border-r-[1px] h-screen">
             <Header/>
             <SearchBar/>
-            <SidebarTask name="Notes" icon={MdNotes} count={noteCount} />
-            <SidebarTask />
-            <SidebarTask />
-            <SidebarTask />
+            {taskObjs.map(obj => <SidebarTask name={obj.name} icon={obj.icon} count={obj.count}/>)}
         </side>
     )
 }

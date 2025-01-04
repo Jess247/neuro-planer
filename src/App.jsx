2150dbe317aebbc1c0e044daa546326093e9c17f
Jess247 <jesss8692@hotmail.com>
2025-01-04 13:21:36 +0100

    Add plus icon to add new task

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,14 @@ function App() {
   const completed = 7
   const progress = completed / totalTasks * 100
 
+  const handleClick = () => {
+    
+  }
+
   return (
     <div className="text-center w-11/12 mx-auto mt-8">
-        <h1 className="text-2xl inline align-middle">Task 1</h1>
-        <FaPlus className="inline float-end" size={25}/>
+      <h1 className="text-2xl inline align-middle">Task 1</h1>
+      <FaPlus className="inline float-end" size={25}/>
       <ProgressBar progress={progress}/>
       <Task/>
     </div>

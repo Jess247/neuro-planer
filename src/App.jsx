84307198c59e10a2467b1177421214561375cf17
Jess247 <jesss8692@hotmail.com>
2025-01-04 12:43:36 +0100

    Add plus icon to add new task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import ProgressBar from './components/ProgressBar'
 import Task from './components/Task'
+import { FaPlus } from "react-icons/fa";
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
 
   return (
     <div className="text-center w-11/12 mx-auto mt-8">
-      <h1 className="text-2xl">Task 1</h1>
+        <h1 className="text-2xl inline align-middle">Task 1</h1>
+        <FaPlus className="inline float-end" size={25}/>
       <ProgressBar progress={progress}/>
       <Task/>
     </div>

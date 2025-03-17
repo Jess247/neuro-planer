0850b75766fc571761686692ba54103f6f5f27dc
Jess247 <jesss8692@hotmail.com>
2025-03-17 16:38:04 +0100

    Add active state to tabs bar

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router"
+import { Link, useLocation } from "react-router"
 function Tabs() {
-    const styles = 'text-[#F76C5E] w-1/2 text-center'
+    const location = useLocation()
+    const { hash, pathname, search } = location
+    
+    const styles = 'text-[#234e70] w-1/2 text-center font-medium'
+    const activeStyles = 'text-[#F76C5E] w-1/2 text-center underline'
     return(
         <nav className="flex justify-around shadow py-5 bg-white">
             <Link 
                 to={{
                     pathname:'/'
                 }} 
-                className={styles + " " + "border-r border-[#F76C5E]"} 
+                className={`${pathname !== '/timer' ? activeStyles : styles} border-r border-[#F76C5E]`} 
                 >Tasks</Link>
             <Link 
-                className={styles}
+                className={pathname === '/timer' ? activeStyles : styles}
                 to={{
                     pathname:'/timer'
                 }} 
